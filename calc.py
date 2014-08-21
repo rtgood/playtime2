96b003436db48866deaf452ecc6ff9a198fa22d9
Rob Good <rtgood@unimelb.edu.au>
2014-08-21 14:49:32 +1000

    Put in the multi

--- calc.py
+++ calc.py
@@ -1,8 +1,14 @@
 import sys
-#def 
 
-#deedede
+def add_all(nums):
+	return sum(nums)
+def multiply_all(nums):
+    return reduce(lambda a, b: a * b, nums)
 
 if __name__ == '__main__':
- 
-	print(sum(map(float, sys.argv[1:])))
+    command = sys.argv[1]
+    nums = map(float, sys.argv[2:])
+    if command == 'add':
+        print(add_all(nums))
+    elif command == 'multiply':
+        print(multiply_all(nums))
